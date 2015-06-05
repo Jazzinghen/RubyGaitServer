c943ffa549e2500011c35b084f8fcd0fdec27187
Michele Bianchi <jazzinghen@gmail.com>
2015-06-05 20:24:32 +0900

    Adding some serious comments

--- GaitServer.rb
+++ GaitServer.rb
@@ -1,25 +1,30 @@
 require 'socket'
 
-puts 'Starting up server...'
-# establish the server
-## Server established to listen for connections on port 15300
-server = TCPServer.new("127.0.0.1", 15300)
-# setup to listen and accept connections
+# Wait, are there style guidelines for Ruby?
+# ESPECIALLY FOR VARIABLE NAMING? O_o
+server_address = "127.0.0.1"
+server_port    = 15300
+
+puts 'Starting up GaitServer'
+
+# Server established to listen for connections on port 15300
+server = TCPServer.new(server_address, server_port)
+
+# Accept a connection, session is a TCPSocket, generated when the server accepts a new connection
 while (session = server.accept)
-  #start new thread conversation
-  ## Here we will establish a new thread for a connection client
+  # Start a new threaded conversation
   Thread.start do
-    ## I want to be sure to output something on the server side
-    ## to show that there has been a connection
-    puts "log: Connection from #{session.peeraddr[2]} at
-          #{session.peeraddr[3]}"
-    puts "log: got input from client"
+    # First things first: we output the information on the client address and port
+    # TCPSocket.peeraddr #=> [address_family, port, hostname and numeric_address]
+    puts "log: Connection from #{session.peeraddr[3]} at
+          #{session.peeraddr[3]}:#{session.peeraddr[1]}"
     ## lets see what the client has to say by grabbing the input
     ## then display it. Please note that the session.gets will look
     ## for an end of line character "\n" before moving forward.
     # input = session.gets
     # puts input
-    ## Lets respond with a nice warm welcome message
+
+    # Let's send something to the client, to acknowledge the connection
     session.puts "Server: Welcome #{session.peeraddr[2]}\n"
     # reply with goodbye
     ## now lets end the session since all we wanted to do is
