d50bd26fd8e7c1bf11c9fc1bb65001e6f3a36b84
Michele Bianchi <jazzinghen@gmail.com>
2015-06-12 11:50:10 +0900

    Integrated AMC parser
    I am also using a float sleep. I have to admit I don't know if that will
    work or not. In the worst case I will have to implement a
    cross-application synch function... Sigh.

--- GaitServer.rb
+++ GaitServer.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'json'
+require_relative 'GaitParsers'
 
 # Wait, are there style guidelines for Ruby?
 # ESPECIALLY FOR VARIABLE NAMING? O_o
@@ -30,16 +31,37 @@ while (session = server.accept)
     puts JSON.generate(angle_data)
 
     session.puts JSON.generate(angle_data)
-    (0..5).each do |i|
-      sleep 1
-      angle = angle + 10
-      angle_data[:left_calf][:roll] = angle
-      session.puts JSON.generate(angle_data)
+    File.open('MOCAP/91_04', 'r') do |mocap_file|
+      mocap_file.each_line do |line|
+        exploded_line = line.split
+        if exploded_line[0].to_i == 0
+          case exploded_line[0]
+            when 'rtibia'
+              angle_data[:right_calf] = tibia_parser(exploded_line.drop(1))
+            when 'ltibia'
+              angle_data[:left_calf] = tibia_parser(exploded_line.drop(1))
+            when 'rfemur'
+              angle_data[:right_thigh] = femur_parser(exploded_line.drop(1))
+            when 'lfemur'
+              angle_data[:left_thigh] = femur_parser(exploded_line.drop(1))
+            when 'rhumerus'
+              angle_data[:right_upperarm] = humerus_parser(exploded_line.drop(1))
+            when 'lhumerus'
+              angle_data[:left_upperarm] = humerus_parser(exploded_line.drop(1))
+            else
+              # Do stuff
+        end
+        else
+          puts JSON.generate(angle_data)
+          session.puts JSON.generate(angle_data)
+          sleep(1.0/30.0)
+        end
+      end
     end
     # reply with goodbye
     ## now lets end the session since all we wanted to do is
     ## acknowledge the client
-    angle_data[:left_calf][:roll] = angle
+    angle_data[:left_calf][:roll] = angle+10
     session.puts JSON.generate(angle_data)
     #session.puts "Server: Goodbye\n"
   end  #end thread conversation
