57fdaa0a229c72d0d7b0ac54452b4bc6c4d079ff
Michele Bianchi <jazzinghen@gmail.com>
2015-06-13 20:15:10 +0900

    Continued testing with AMC
    I will now try to work with another format, closer to Unreal Engine way
    of managing angles...

--- AMCParseTest.rb
+++ AMCParseTest.rb
@@ -1,5 +1,5 @@
 require 'json'
-require_relative 'GaitParsers'
+require_relative 'GaitAMCParsers'
 
 angle_data = {}
 
@@ -20,10 +20,65 @@ File.open('MOCAP/91_04.amc', 'r') do |mocap_file|
           angle_data[:right_upperarm] = humerus_parser(exploded_line.drop(1))
         when 'lhumerus'
           angle_data[:left_upperarm] = humerus_parser(exploded_line.drop(1))
+        when 'lowerback'
+          angle_data[:spine_01] = spine_parser(exploded_line.drop(1))
+        when 'upperback'
+          angle_data[:spine_02] = spine_parser(exploded_line.drop(1))
+        when 'thorax'
+          angle_data[:spine_03] = spine_parser(exploded_line.drop(1))
+        when 'upperneck'
+          angle_data[:neck] = neck_parser(exploded_line.drop(1))
+        when 'head'
+          angle_data[:head] = neck_parser(exploded_line.drop(1))
+        when 'root'
+          angle_data[:root] = root_parser(exploded_line.drop(4))
+        when 'lhipjoint'
+          angle_data[:left_hip_joint] = hip_parser(exploded_line.drop(1))
+        when 'rhipjoint'
+          angle_data[:right_hip_joint] = hip_parser(exploded_line.drop(1))
         else
           # Do stuff
       end
     else
+
+
+      if angle_data[:left_hip_joint]
+        angle_data[:left_hip_joint].each do |key, value|
+          angle_data[:left_hip_joint][key] = value - angle_data[:root][key]
+        end
+      end
+
+      if angle_data[:right_hip_joint]
+        angle_data[:right_hip_joint].each do |key, value|
+          angle_data[:right_hip_joint][key] = value - angle_data[:root][key]
+        end
+      end
+
+      if angle_data[:left_thigh]
+        angle_data[:left_thigh].each do |key, value|
+          angle_data[:left_thigh][key] = value - angle_data[:root][key]
+        end
+      end
+
+      if angle_data[:right_thigh]
+        angle_data[:right_thigh].each do |key, value|
+          angle_data[:right_thigh][key] = value - angle_data[:root][key]
+        end
+      end
+
+      if angle_data[:left_calf]
+        angle_data[:left_calf].each do |key, value|
+          angle_data[:left_calf][key] = value - angle_data[:left_thigh][key]
+        end
+      end
+
+      if angle_data[:right_calf]
+        angle_data[:right_calf].each do |key, value|
+          angle_data[:right_calf][key] = value - angle_data[:right_thigh][key]
+        end
+      end
+
+
       puts JSON.generate(angle_data)
     end
   end
--- GaitServer.rb
+++ GaitServer.rb
@@ -1,6 +1,6 @@
 require 'socket'
 require 'json'
-require_relative 'GaitParsers'
+require_relative 'GaitAMCParsers'
 
 # Wait, are there style guidelines for Ruby?
 # ESPECIALLY FOR VARIABLE NAMING? O_o
@@ -26,35 +26,54 @@ while (session = server.accept)
     ## for an end of line character "\n" before moving forward.
 
     # Generate a very basic JSON structure to send to UE4
-    angle_data = {left_calf: {pitch: 0, yaw: 0, roll: angle}}
+    angle_data = {} #{left_calf: {pitch: 0, yaw: 0, roll: angle}}
 
-    puts JSON.generate(angle_data)
+    #puts JSON.generate(angle_data)
 
-    session.puts JSON.generate(angle_data)
-    File.open('MOCAP/91_04', 'r') do |mocap_file|
+    i = 0
+
+    #session.puts JSON.generate(angle_data)
+    File.open('MOCAP/08_02.amc', 'r') do |mocap_file|
       mocap_file.each_line do |line|
         exploded_line = line.split
         if exploded_line[0].to_i == 0
           case exploded_line[0]
-            when 'rtibia'
-              angle_data[:right_calf] = tibia_parser(exploded_line.drop(1))
             when 'ltibia'
+              angle_data[:right_calf] = tibia_parser(exploded_line.drop(1))
+            when 'rtibia'
               angle_data[:left_calf] = tibia_parser(exploded_line.drop(1))
-            when 'rfemur'
-              angle_data[:right_thigh] = femur_parser(exploded_line.drop(1))
             when 'lfemur'
+              angle_data[:right_thigh] = femur_parser(exploded_line.drop(1))
+            when 'rfemur'
               angle_data[:left_thigh] = femur_parser(exploded_line.drop(1))
             when 'rhumerus'
               angle_data[:right_upperarm] = humerus_parser(exploded_line.drop(1))
             when 'lhumerus'
               angle_data[:left_upperarm] = humerus_parser(exploded_line.drop(1))
+            when 'lowerback'
+              angle_data[:spine_01] = spine_parser(exploded_line.drop(1))
+            when 'upperback'
+              angle_data[:spine_02] = spine_parser(exploded_line.drop(1))
+            when 'thorax'
+              angle_data[:spine_03] = spine_parser(exploded_line.drop(1))
+            when 'upperneck'
+              angle_data[:neck] = neck_parser(exploded_line.drop(1))
+            when 'head'
+              angle_data[:head] = neck_parser(exploded_line.drop(1))
+            when 'root'
+              angle_data[:root] = root_parser(exploded_line.drop(4))
+            when 'lhipjoint'
+              angle_data[:left_hip_joint] = hip_parser(exploded_line.drop(1))
+            when 'rhipjoint'
+              angle_data[:right_hip_joint] = hip_parser(exploded_line.drop(1))
             else
               # Do stuff
-        end
+          end
         else
+
           puts JSON.generate(angle_data)
           session.puts JSON.generate(angle_data)
-          sleep(1.0/30.0)
+          sleep(0.05)
         end
       end
     end
